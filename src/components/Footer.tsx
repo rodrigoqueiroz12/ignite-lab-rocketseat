cca53bd4b39d49ff349e5134bdf2afdc42fbd0e0
Rodrigo <rodrigo.queiroz0629@gmail.com>
2022-06-26 22:25:51 -0300

    Ajuste no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { RocketseatLogo } from "./RocketseatLogo";
 
 export function Footer() {
   return (
-    <div className="w-full flex-1 flex flex-col gap-7 md:flex-row items-center bg-gray-900 p-7 md:py-5">
+    <div className="w-full flex-1 flex flex-col gap-7 md:flex-row md:max-h-[75px] items-center bg-gray-900 p-7 md:py-5">
       <RocketseatLogo />
       <span className="text-md text-gray-300">Rocketseat - Todos os direitos reservados</span>
       <span className="text-md text-gray-300 md:flex-1 text-end">Políticas de privacidade</span>
